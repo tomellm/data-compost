77da61dae50022e71c7fa28a5bb662eab36093fd
EmmaHny <84329668+EmmaHny@users.noreply.github.com>
2022-05-17 18:58:05 +0200

    loots of change- animations
    changed asthetics into black, added text in the middle, changed header, added stirMe button, added an index.css file and moved all the inline css, added keyframes for animations in css, added animation selector to js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const dropzone = document.querySelector('main'); 
+var animationSelector = 0;
 
 dropzone.addEventListener('dragenter', event => { 
     event.preventDefault();
@@ -24,12 +25,13 @@ dropzone.addEventListener('drop', event => {
     
         const file = event.dataTransfer.files[step];
         const reader = new FileReader(); // File reader= class, reader= object 
+        
+        console.log(file.type);
         reader.readAsDataURL(file);
-        console.log(event.dataTransfer.files)
 
         reader.addEventListener('loadend', () => {
             const img = document.createElement('img');
-            img.src = reader.result;
+            img.src = reader.result;    
            
             var x = Math.random()*(dropzone.offsetWidth-100);
             var y = Math.random()*(dropzone.offsetHeight-100);
@@ -37,12 +39,141 @@ dropzone.addEventListener('drop', event => {
             img.style.top= y + 'px';
 
             dropzone.append(img);
+            
+            //width: calc((100vw - 35px) / 10);
+            //height: calc((100vw - 35px) / 10);
 
+            var width = img.offsetWidth; //5
+            var height = img.offsetHeight; //4
+            
+
+            if(width > height) { //true
+                height = height / width; // 0,8
+                width = 1;
+            } else if(height > width) {
+                width = width / height;
+                height = 1;
+            } else {
+                height = 1;
+                width = 1;
+            }
+
+            
+            
+           
+         animationSelector = Math.floor(Math.random() * 11);
+            
+         if (animationSelector == 0) {
+           
+            img.style.position = "absolute";
+            img.style.width = "calc(100px * " + width + ")"
+            img.style.height = "calc(100px * " + height + ")";
+            img.style.transform = "rotate(0deg) skewY(0deg)";
+            img.style.animationName = "transform";
+            img.style.animationDuration = Math.floor(Math.random() * 100)+"s";
+            img.style.animationIterationCount = "infinite";
+            }
+
+        if (animationSelector == 1) {
+           
+            img.style.position = "absolute";
+            img.style.width = "300px" ;
+            img.style.height = "300px";
+            img.style.animationName = "feGaussianBlur";
+            img.style.animationDuration = Math.floor(Math.random() * 200)+"s";
+            }
+
+        if (animationSelector == 2) {
+           
+            img.style.width = "30px" ;
+            img.style.height = "30px";
+            img.style.animationName = "transform";
+            img.style.animationDuration = Math.floor(Math.random() * 100)+"s";
+            img.style.animationIterationCount = "infinite";
+            }
+       
+        if (animationSelector == 3) {
+           
+            img.style.width = "30px" ;
+            img.style.height = "30px";
+            img.style.animationName = "transform";
+            img.style.animationDuration = Math.floor(Math.random() * 100)+"s";
+            img.style.animationIterationCount = "infinite";
+            }    
+        if (animationSelector == 4) {
+           
+            img.style.width = "30px" ;
+            img.style.height = "30px";
+            img.style.animationName = "transform";
+            img.style.animationDuration = Math.floor(Math.random() * 100)+"s";
+            img.style.animationIterationCount = "infinite";
+            }
+    
+        if (animationSelector == 5) {
+               
+            img.style.width = "30px" ;
+            img.style.height = "30px";
+            img.style.animationName = "transform";
+            img.style.animationDuration = Math.floor(Math.random() * 100)+"s";
+            img.style.animationIterationCount = "infinite";
+            }
+           
+        if (animationSelector == 6) {
+               
+            img.style.width = "30px" ;
+            img.style.height = "30px";
+            img.style.animationName = "transform";
+            img.style.animationDuration = Math.floor(Math.random() * 100)+"s";
+            img.style.animationIterationCount = "infinite";
+            }
+
+        if (animationSelector == 7) {
+           
+            img.style.width = "30px" ;
+            img.style.height = "30px";
+            img.style.animationName = "transform";
+            img.style.animationDuration = Math.floor(Math.random() * 100)+"s";
+            img.style.animationIterationCount = "infinite";
+            }
+    
+        if (animationSelector == 8) {
+               
+            img.style.width = "30px" ;
+            img.style.height = "30px";
+            img.style.animationName = "transform";
+            img.style.animationDuration = Math.floor(Math.random() * 100)+"s";
+            img.style.animationIterationCount = "infinite";
+            }
+           
+        if (animationSelector == 9) {
+               
+            img.style.width = "30px" ;
+            img.style.height = "30px";
+            img.style.animationName = "transform";
+            img.style.animationDuration = Math.floor(Math.random() * 100)+"s";
+            img.style.animationIterationCount = "infinite";
+            }    
+            
+        if (animationSelector == 10) {
+               
+                img.style.width = "30px" ;
+                img.style.height = "30px";
+                img.style.animationName = "transform";
+                img.style.animationDuration = Math.floor(Math.random() * 100)+"s";
+                img.style.animationIterationCount = "infinite";
+                }
+        
+        if (animationSelector == 11) {
+                   
+                img.style.width = "30px" ;
+                img.style.height = "30px";
+                img.style.animationName = "transform";
+                img.style.animationDuration = Math.floor(Math.random() * 100)+"s";
+                img.style.animationIterationCount = "infinite";
+                }
+            
         });   
-
     };
-    
-    
 });
 
 
