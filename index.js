29259d3d14a5647f2a08003b8ed45b4d1e9c8508
tomellm <thomas.ellmenreich@gmail.com>
2022-05-29 19:49:58 +0200

    fixed real compost

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
 const dropzone = document.querySelector('main'); 
 var animationSelector = 0;
 
+//Array that will contain
+let pixels = new Array(10).fill(new Array(180000));
+const SECTORSIZE = 10;
+const DIMENSIONS = 4;
+
+class Kastl {
+    /*
+    [
+        [ ... ],
+        [ ... ],
+        [ ... ],
+        [ ... ],
+        [ ... ]
+    ]
+    */
+
+    constructor() {
+        this.pointer = 0;
+        this.data = new Array(SECTORSIZE * DIMENSIONS);
+    }
+
+    update(line) {
+        data[pointer] = line;
+        pointer++;
+    }
+
+    update(line, num) {
+        data[num] = line;
+    }
+
+}
+
+
 dropzone.addEventListener('dragenter', event => { 
     event.preventDefault();
     dropzone.classList.add('active');
@@ -25,30 +58,27 @@ dropzone.addEventListener('drop', event => {
     
         const file = event.dataTransfer.files[step];
         const reader = new FileReader(); // File reader= class, reader= object 
-        
-        console.log(file.type);
+    
         reader.readAsDataURL(file);
 
         reader.addEventListener('loadend', () => {
             const img = document.createElement('img');
             img.src = reader.result;    
            
-            var x = Math.random()*(dropzone.offsetWidth-100);
-            var y = Math.random()*(dropzone.offsetHeight-100);
+            const x = Math.random()*(dropzone.offsetWidth-100);
+            const y = Math.random()*(dropzone.offsetHeight-100);
             img.style.left = x + 'px';
             img.style.top= y + 'px';
 
-            dropzone.append(img);
-            
             //width: calc((100vw - 35px) / 10);
             //height: calc((100vw - 35px) / 10);
 
-            var width = img.offsetWidth; //5
-            var height = img.offsetHeight; //4
+            var width = img.offsetWidth;
+            var height = img.offsetHeight;
             
 
-            if(width > height) { //true
-                height = height / width; // 0,8
+            if(width > height) {
+                height = height / width;
                 width = 1;
             } else if(height > width) {
                 width = width / height;
@@ -59,123 +89,148 @@ dropzone.addEventListener('drop', event => {
             }
 
             
-        animationSelector = 5 //Math.floor(Math.random() * 11);
+            animationSelector = Math.floor(Math.random() * 11);
          
-        img.style.position = "absolute";
-        img.style.width = "calc(100px * " + width + ")"
-        img.style.height = "calc(100px * " + height + ")";
-         
-         if (animationSelector == 0) {
-           
+            img.style.position = "absolute";
             img.style.width = "calc(100px * " + width + ")"
             img.style.height = "calc(100px * " + height + ")";
-            img.style.transform = "rotate(0deg) skewY(0deg)";
-            img.style.animationName = "transform";
-            img.style.animationDuration = Math.floor(Math.random() * 100)+"s";
-            img.style.animationIterationCount = "infinite";
-            }
-
-        if (animationSelector == 1) {
-            img.style.position = "absolute";
-            img.style.width = "400px" ;
-            img.style.height = "300px";
-            img.style.animationName = "feGaussianBlur";
-            img.style.animationDuration = Math.floor(Math.random() * 200)+"s";
-            }
-
-        if (animationSelector == 2) {
-            img.style.position = "absolute";
-            img.style.width = "600px" ;
-            img.style.height = "200px";
-            img.style.animationName = "invert";
-            img.style.animationDuration = Math.floor(Math.random() * 200)+"s";
-            img.style.filter = "invert(1)"
-            }
-       
-       if (animationSelector == 3) {
-           
-            img.style.width = "30px" ;
-            img.style.height = "30px";
-            img.style.animationName = "opacity";
-            img.style.animationDuration = Math.floor(Math.random() * 100)+"s";
-            img.style.animationIterationCount = 1;
-            img.style.filter = "opacity(0)"
-            }    
-        if (animationSelector == 4) {
-           
-            img.style.width = "5" ;
-            img.style.height = "60";
-            img.style.animationName = "transform2";
-            img.style.animationDuration = Math.floor(Math.random() * 5000)+"s";
-            img.style.animationIterationCount = 1;
-            img.style.transform = scale(1000, 0.5);
-            }
-    
-        if (animationSelector == 5) {
-               
-            img.style.width = "200px" ;
-            img.style.height = "200px";
-            img.style.animationName = "";
-            img.style.animationDuration = Math.floor(Math.random() * 10)+"s";
-            img.style.animationIterationCount = 1;
-            }
-           
-        if (animationSelector == 6) {
-               
-            img.style.width = "30px" ;
-            img.style.height = "30px";
-            img.style.animationName = "transform";
-            img.style.animationDuration = Math.floor(Math.random() * 100)+"s";
-            img.style.animationIterationCount = "infinite";
-            }
-
-        if (animationSelector == 7) {
-           
-            img.style.width = "30px" ;
-            img.style.height = "30px";
-            img.style.animationName = "transform";
-            img.style.animationDuration = Math.floor(Math.random() * 100)+"s";
-            img.style.animationIterationCount = "infinite";
-            }
-    
-        if (animationSelector == 8) {
-               
-            img.style.width = "30px" ;
-            img.style.height = "30px";
-            img.style.animationName = "transform";
-            img.style.animationDuration = Math.floor(Math.random() * 100)+"s";
-            img.style.animationIterationCount = "infinite";
-            }
-           
-        if (animationSelector == 9) {
-               
-            img.style.width = "30px" ;
-            img.style.height = "30px";
-            img.style.animationName = "transform";
-            img.style.animationDuration = Math.floor(Math.random() * 100)+"s";
-            img.style.animationIterationCount = "infinite";
-            }    
-            
-        if (animationSelector == 10) {
-               
-                img.style.width = "30px" ;
-                img.style.height = "30px";
-                img.style.animationName = "transform";
-                img.style.animationDuration = Math.floor(Math.random() * 100)+"s";
+         
+            if (animationSelector == 0) {
+                console.log(animationSelector)
+                setImageAnimation(img, "calc(100px * " + width + ")", "calc(100px * " + height + ")", "transform1", Math.floor(Math.random() * 100)+"s", "infinite");
+                img.style.transform = "rotate(0deg) skewY(0deg)";
                 img.style.animationIterationCount = "infinite";
+                dropzone.append(img);
+            }else if (animationSelector == 1) {
+                console.log(animationSelector)
+                img.style.position = "absolute";
+                setImageAnimation(img, "400px", "300px", "feGaussianBlur", Math.floor(Math.random() * 200)+"s", "auto");
+                dropzone.append(img);
+            }else if (animationSelector == 2) {
+                console.log(animationSelector)
+                setImageAnimation(img, "600px", "300px", "invert", Math.floor(Math.random() * 200)+"s", "auto");
+                img.style.position = "absolute";
+                img.style.filter = "invert(1)"
+                dropzone.append(img);
+            } else if (animationSelector == 3) {
+                console.log(animationSelector)
+                setImageAnimation(img, "30px", "30px", "opacity", Math.floor(Math.random() * 100)+"s", "1");
+                img.style.filter = "opacity(0)"
+                dropzone.append(img);
+            } else if (animationSelector == 4) {
+                console.log(animationSelector)
+                setImageAnimation(img, "5px", "60px", "transform2", Math.floor(Math.random() * 5000)+"s", "1");
+                img.style.transform = "scale(1000, 0.5)";
+                dropzone.append(img);
+            } else if (animationSelector >= 5 && animationSelector <= 12) {
+                let image = new Image();
+                image.src = img.src;
+                image.onload = function() {
+                    const canvas = document.getElementById('canvas');
+                    const virtCanvas = document.getElementById('virt-canvas');
+                    
+                    const imageWidth = makeDevidableByTen(Math.random() * canvas.width);
+                    const imageHeight = makeDevidableByTen(Math.random() * canvas.height);
+
+                    const leftPos = makeDevidableByTen(Math.random() * (canvas.width - imageWidth));
+                    const topPos = makeDevidableByTen(Math.random() * (canvas.height - imageHeight));
+                    
+                    var ctx = canvas.getContext('2d'); 
+                    var virtCtx = virtCanvas.getContext('2d');
+
+                    virtCtx.drawImage(image, leftPos, topPos, imageWidth, imageHeight);
+
+                    var virtImageData = virtCtx.getImageData(leftPos, topPos, imageWidth, imageHeight);
+                    var imageData = ctx.getImageData(leftPos, topPos, imageWidth, imageHeight);
+
+                    manipulatePixels(virtImageData, imageData)
+                    
+                    ctx.putImageData(imageData, leftPos, topPos)
                 }
-        
-        if (animationSelector == 11) {
-                   
-                img.style.width = "30px" ;
-                img.style.height = "30px";
-                img.style.animationName = "transform";
-                img.style.animationDuration = Math.floor(Math.random() * 100)+"s";
-                img.style.animationIterationCount = "infinite";
-                }
-            
+            }
         });   
     };
 });
 
+//function that takes a image as a parameter with all of the different properties, assigns them to the image
+//made to compact code and reduce repeated code
+function setImageAnimation(img, width, height, animationName, animationDuration, animationIterationCount) {
+    img.style.width = width;
+    img.style.height = height;
+    img.style.animationName = animationName;
+    img.style.animationDuration = animationDuration;
+    img.style.animationIterationCount = animationIterationCount;
+    img.style.opacity = Math.random()
+}
+
+//data: is the array of channels for all of the pixels of a single image
+//width and height: size of the image in pixels not in channels
+function manipulatePixels(newImage, targetData) {
+    const width = newImage.width;
+    const height = newImage.height;
+    const deletes = new Array(height / SECTORSIZE);
+
+    for(let i = 0; i < deletes.length; i++) {
+        deletes[i] = new Array(width / SECTORSIZE);
+        for(let j = 0; j < deletes[i].length; j++) {
+            deletes[i][j] = Math.random() > 0.5;
+        }
+    }
+    
+    /*
+        [
+            [new Kastl, new Kastl, new Kastl, new Kastl, new Kastl],
+            [],
+            [],
+            [],
+            []
+        ]
+    */
+    
+    let currentX = 0, currentY = 0;
+    const newArray = new Array(newImage.data.length);
+    for(
+        let i = 0;
+        i < (height / SECTORSIZE) * (width * DIMENSIONS * SECTORSIZE);
+        i += (width * DIMENSIONS * SECTORSIZE)
+    ) {
+        for(
+            let h = i; 
+            h < width * DIMENSIONS * SECTORSIZE + i;
+            h += width * DIMENSIONS
+        ) {
+            for(
+                let k = h;
+                k < (width * DIMENSIONS) + h;
+                k += SECTORSIZE * DIMENSIONS
+            ) {
+                if(deletes[currentY][currentX]) {
+                    for(let l = k; l < k + (SECTORSIZE * DIMENSIONS); l++) {
+                        targetData.data[l] = newImage.data[l];
+                    }
+                }
+                currentX++;
+            }
+            currentX = 0;
+        }
+        currentY++;
+        currentX = 0;
+    }
+}
+
+
+function makeDevidableByTen(number) {
+    if(number % 10 == 0) {
+        return number
+    }
+    const final = number - number % 10
+    if(final < 0) {
+        return 0;
+    }
+    return final;
+}
+/*
+const interval = setInterval(function() {
+    console.log("hello");
+}, 5000);*/
 
